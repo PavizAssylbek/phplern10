b2a54b684688c9b2d7d182961d4f11b11bf4651e
Asyl Qwerty <pa@crocos.kz>
2021-01-21 18:00:17 +0600

    fix

--- task_2.php
+++ task_2.php
@@ -41,23 +41,14 @@
                           We will only use your personal information to administer your account, provide the products and services you have requested from us, and to keep you informed about our products and services (if you have consented to this).
                           We only use your data for the purposes for which it was collected and, where relevant, to meet local legal obligations.
                           We will retain your personal information only for as long as is necessary for the purposes for which the information was collected, or as long as is required pursuant to law.",
-                          'class' => 'mb-g',
-                          'h2' => true
                       ],[
                           "title" => "Cookies and other similar technologies",
                           "text" => "We collect certain data through cookies and similar technologies (e.g. web beacons, tags, device identifiers). Cookies are text files placed on your computer to collect standard internet log information and visitor behaviour information. This information is used to track visitor use of the website and to compile statistical reports on website activity. We register your interaction with our services in order to improve our website, content and services. Our use of such technologies and the data collected is described in more detail in our Cookie Policy. You can manage your cookie preferences through your browser settings.",
-                          'class' => null,
-                          'h2' => false
                       ]
                   ];
                   foreach ($items as $key => $obj) {
-                    if($obj['h2']) {
-                        echo '<h2>'.$obj['title'].'</h2>';
-                        echo '<p class="'.$obj['class'].'">'.$obj['text'].'</p>';
-                    } else {
-                        echo '<h3>'.$obj['title'].'</h3>';
-                        echo '<p class="'.$obj['class'].'">'.$obj['text'].'</p>';
-                    }
+                    echo '<h2>'.$obj['title'].'</h2>';
+                    echo '<p class="mb-g">'.$obj['text'].'</p>';
                   }
                   ?>
                             <!-- <h2>Privacy</h2>
